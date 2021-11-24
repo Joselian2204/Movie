1f34f35794c3d7d3a21cb04df3b71fc4b8509e73
Joselian2204 <josemar.elian.castro.paz@gmail.com>
2021-11-24 15:56:43 -0400

    Carousel finished

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
 import Cards from './cards.js';
 import Checker from './checker.js';
-import Carousel from "./carousel.js";
 
 
 window.addEventListener("DOMContentLoaded",() => {
     const submitBtn = document.querySelector(".btn-search")
 
     const cards = new Cards();
-    //const carousel = new Carousel();
-
-    //carousel.showCarouselInfo();
+    
     cards.getMoviesExample();
 
     submitBtn.addEventListener("click", (e) => {
